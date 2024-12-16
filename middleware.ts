1f83feb0ade6965355770577d3cd769aa4c50a0f
gabrielnfc <gabrielnfc@gmail.com>
2024-12-16 13:16:53 -0300

    Corrigindo autenticaçaõ

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,9 @@
-import { NextResponse } from 'next/server';
-import { getAuth } from '@clerk/nextjs/server';
-import type { NextRequest } from 'next/server';
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default async function middleware(req: NextRequest) {
-  const { userId } = await getAuth(req);
-
-  if (!userId && !req.url.includes('/sign-in')) {
-    const signInUrl = new URL('/sign-in', req.url);
-    return NextResponse.redirect(signInUrl);
-  }
-
-  if (userId && req.url.includes('/sign-in')) {
-    const homeUrl = new URL('/', req.url);
-    return NextResponse.redirect(homeUrl);
-  }
-
-  return NextResponse.next();
-}
+export default authMiddleware({
+  publicRoutes: ["/sign-in"]
+});
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-  runtime: "nodejs",
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
